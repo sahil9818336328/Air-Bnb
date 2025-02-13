ef7e7e09f44a4c2b104c2eaa978a8d9572e7428f
sahil khatri <s.khatri@boutiqaat.com>
2025-02-13 11:09:05 +0530

    navbar complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+import Navbar from '@/components/navbar/Navbar'
+import Providers from './providers'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -24,11 +26,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          <Navbar />
+        </Providers>
+        <main className='container py-10'>{children}</main>
       </body>
     </html>
   )
--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { SiAirbnb } from 'react-icons/si'
+import Link from 'next/link'
+import { Button } from '../ui/button'
 
 const Logo = () => {
-  return <div>Logo</div>
+  return (
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <SiAirbnb className='w-6 h-6' />
+      </Link>
+    </Button>
+  )
 }
 
 export default Logo
