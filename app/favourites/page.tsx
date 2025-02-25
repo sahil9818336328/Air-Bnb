cd4cf1491782d5150b5e3fb8aaad51fadd6ebe73
sahil khatri <s.khatri@boutiqaat.com>
2025-02-25 11:36:01 +0530

    add to favorite and search functionality

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import EmptyList from '@/components/home/EmptyList'
+import PropertiesList from '@/components/home/PropertiesList'
+import { fetchFavorites } from '@/utils/actions'
 
-const FavouritesPage = () => {
-  return <div>FavouritesPage</div>
+async function FavoritesPage() {
+  const favorites = await fetchFavorites()
+
+  if (favorites.length === 0) {
+    return <EmptyList />
+  }
+
+  return <PropertiesList properties={favorites} />
 }
-
-export default FavouritesPage
+export default FavoritesPage
--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,10 +1,12 @@
-import { FaHeart } from 'react-icons/fa'
-import { Button } from '@/components/ui/button'
-function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
-  return (
-    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-      <FaHeart />
-    </Button>
-  )
+import { auth } from '@clerk/nextjs/server'
+import { CardSignInButton } from '../form/Buttons'
+import { fetchFavoriteId } from '@/utils/actions'
+import FavoriteToggleForm from './FavoriteToggleForm'
+async function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+  const { userId } = auth()
+  if (!userId) return <CardSignInButton />
+  const favoriteId = await fetchFavoriteId({ propertyId })
+
+  return <FavoriteToggleForm favoriteId={favoriteId} propertyId={propertyId} />
 }
 export default FavoriteToggleButton
