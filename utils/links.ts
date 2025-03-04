4b6857957b4bc120a2c6432feb3d667737015f4a
sahil khatri <s.khatri@boutiqaat.com>
2025-03-04 10:15:20 +0530

    rentals and reservations page

--- utils/links.ts
+++ utils/links.ts
@@ -5,10 +5,12 @@ type NavLink = {
 
 export const links: NavLink[] = [
   { href: '/', label: 'home' },
-  { href: '/favourites ', label: 'favorites' },
+  { href: '/favorites ', label: 'favorites' },
   { href: '/bookings ', label: 'bookings' },
   { href: '/reviews ', label: 'reviews' },
+  { href: '/reservations ', label: 'reservations' },
   { href: '/rentals/create ', label: 'create rental' },
   { href: '/rentals', label: 'my rentals' },
+  { href: '/admin', label: 'admin' },
   { href: '/profile ', label: 'profile' },
 ]
