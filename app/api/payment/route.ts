b640ded48f0031d4a135c3e820f6fd24a9e477e8
sahil khatri <s.khatri@boutiqaat.com>
2025-03-05 10:35:01 +0530

    removed un-used code, vercel deployment error

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,9 +1,9 @@
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
-import { type NextRequest, type NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 import db from '@/utils/db'
 import { formatDate } from '@/utils/format'
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
   const requestHeaders = new Headers(req.headers)
   const origin = requestHeaders.get('origin')
 
